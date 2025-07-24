0b08bd6a75ccb25b5ae6940e2c45b87f2380719a
tonyhan18 <ourclub7279@gmail.com>
2025-07-24 20:43:31 +0900

    FEAT : Product 컨트롤러 추가

---
+++ src/main/java/kr/hhplus/be/server/interfaces/products/ProductController.java
@@ -0,0 +1,28 @@
+package kr.hhplus.be.server.interfaces.products;
+
+import org.springframework.web.bind.annotation.*;
+
+import kr.hhplus.be.server.application.product.ProductFacade;
+import kr.hhplus.be.server.application.product.ProductResult;
+import kr.hhplus.be.server.interfaces.ApiResponse;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/products")
+@RequiredArgsConstructor
+public class ProductController {
+    
+    private final ProductFacade productFacade;
+
+    @GetMapping("")
+    public ApiResponse<ProductResponse.Products> getProducts() {
+        ProductResult.Products products = productFacade.getProducts();
+        return ApiResponse.success(ProductResponse.Products.of(products));
+    }
+
+    @GetMapping("/ranks")
+    public ApiResponse<ProductResponse.Products> getPopularProducts() {
+        ProductResult.Products products = productFacade.getPopularProducts();
+        return ApiResponse.success(ProductResponse.Products.of(products));
+    }
+} 
