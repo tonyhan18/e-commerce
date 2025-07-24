e6f9c780a51adb0685c1eb924e2cd6c39a136724
tonyhan18 <ourclub7279@gmail.com>
2025-07-25 03:17:22 +0900

    FEAT : UserCoupong Domain 기능 추가

---
+++ src/main/java/kr/hhplus/be/server/domain/user/UserCouponInfo.java
@@ -0,0 +1,56 @@
+package kr.hhplus.be.server.domain.user;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class UserCouponInfo {
+
+    @Getter
+    public static class UsableCoupon {
+
+        private final Long userCouponId;
+
+        private UsableCoupon(Long userCouponId) {
+            this.userCouponId = userCouponId;
+        }
+
+        public static UsableCoupon of(Long userCouponId) {
+            return new UsableCoupon(userCouponId);
+        }
+    }
+
+    @Getter
+    public static class Coupons {
+
+        private final List<Coupon> coupons;
+
+        private Coupons(List<Coupon> coupons) {
+            this.coupons = coupons;
+        }
+
+        public static Coupons of(List<Coupon> coupons) {
+            return new Coupons(coupons);
+        }
+    }
+
+    @Getter
+    public static class Coupon {
+
+        private final Long userCouponId;
+        private final Long couponId;
+        private final LocalDateTime issuedAt;
+
+        @Builder
+        private Coupon(Long userCouponId, Long couponId, LocalDateTime issuedAt) {
+            this.userCouponId = userCouponId;
+            this.couponId = couponId;
+            this.issuedAt = issuedAt;
+        }
+    }
+}
