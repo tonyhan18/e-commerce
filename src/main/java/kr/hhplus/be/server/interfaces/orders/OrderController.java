6aa2ac3709b12b8b569ca110cb2e9bdab43916c6
tonyhan18 <ourclub7279@gmail.com>
2025-07-24 20:40:09 +0900

    FEAT : Order 컨트롤러 추가

---
+++ src/main/java/kr/hhplus/be/server/interfaces/orders/OrderController.java
@@ -0,0 +1,24 @@
+package kr.hhplus.be.server.interfaces.orders;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+import kr.hhplus.be.server.application.order.OrderFacade;
+import kr.hhplus.be.server.interfaces.ApiResponse;
+import jakarta.validation.Valid;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/orders")
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderFacade orderFacade;
+
+    @PostMapping("")
+    public ApiResponse<Void> orderPayment(@Valid @RequestBody OrderRequest.OrderPayment request) {
+        orderFacade.orderPayment(request.toCriteria()); 
+        return ApiResponse.success();
+    }
+} 
