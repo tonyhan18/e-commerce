6f5650c55f4641e43e7988f9bd97dc7da877a2ac
tonyhan18 <ourclub7279@gmail.com>
2025-07-28 07:39:30 +0900

    FEAT : Balance 클래스 JPA 연결

--- src/main/java/kr/hhplus/be/server/application/balance/BalanceFacade.java
+++ src/main/java/kr/hhplus/be/server/application/balance/BalanceFacade.java
@@ -1,5 +1,6 @@
 package kr.hhplus.be.server.application.balance;
 
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Component;
 
 import kr.hhplus.be.server.domain.balance.BalanceService;
@@ -14,11 +15,13 @@ public class BalanceFacade {
     private final UserService userService;
     private final BalanceService balanceService;
 
+    @Transactional
     public void chargeBalance(BalanceCriteria.Charge criteria) {
         userService.getUser(criteria.getUserId());
         balanceService.chargeBalance(criteria.toCommand());
     }
 
+    @Transactional(readOnly = true)
     public BalanceResult.Balance getBalance(Long userId) {
         userService.getUser(userId);
         BalanceInfo.Balance balance = balanceService.getBalance(userId);
