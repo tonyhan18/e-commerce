cbefe14bcd270a4ff4d4072efeba28b62edc501d
tonyhan18 <ourclub7279@gmail.com>
2025-08-08 07:06:17 +0900

    REFACTOR : 미사용 테스트 support 제거 및 미사용 파사드 동시성 테스트 삭제

--- src/test/java/kr/hhplus/be/server/MockTestSupport.java
+++
@@ -1,8 +0,0 @@
-package kr.hhplus.be.server;
-
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-@ExtendWith(MockitoExtension.class)
-public abstract class MockTestSupport {
-}
