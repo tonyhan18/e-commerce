83488c82358660546d6ea2820f9dc52f55c42d8b
tonyhan18 <ourclub7279@gmail.com>
2025-08-08 05:45:26 +0900

    REFACTOR : 쿠폰 비관적 락 동시성 제어 구현

--- src/main/java/kr/hhplus/be/server/domain/coupon/CouponService.java
+++ src/main/java/kr/hhplus/be/server/domain/coupon/CouponService.java
@@ -2,6 +2,7 @@ package kr.hhplus.be.server.domain.coupon;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -9,9 +10,11 @@ public class CouponService {
 
     private final CouponRepository couponRepository;
 
+    @Transactional
     public void publishCoupon(Long couponId) {
-        Coupon coupon = couponRepository.findById(couponId);
+        Coupon coupon = couponRepository.findWithLockById(couponId);
         coupon.publish();
+        couponRepository.save(coupon); // 변경사항을 데이터베이스에 저장
     }
 
     public CouponInfo.Coupon getCoupon(Long couponId) {
