b5f9e58d8dc89efa1b4eae857593d1ca519df0d8
tonyhan18 <ourclub7279@gmail.com>
2025-08-08 05:39:04 +0900

    FEAT : 테스트를 위한 서포트 파일 제작

---
+++ src/test/java/kr/hhplus/be/server/support/IntegrationTestSupport.java
@@ -0,0 +1,10 @@
+package kr.hhplus.be.server.support;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public abstract class IntegrationTestSupport {
+
+}
---
+++ src/test/java/kr/hhplus/be/server/support/MockTestSupport.java
@@ -0,0 +1,8 @@
+package kr.hhplus.be.server.support;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public abstract class MockTestSupport {
+}
