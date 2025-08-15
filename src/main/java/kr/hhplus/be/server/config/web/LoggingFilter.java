3079ec428be0cfd06effa51ca51b112de0f75411
tonyhan18 <ourclub7279@gmail.com>
2025-08-15 21:16:19 +0900

    FEAT : 레디스, 스케쥴러, 로깅 필터, DB Transaction 추가

---
+++ src/main/java/kr/hhplus/be/server/config/web/LoggingFilter.java
@@ -0,0 +1,72 @@
+package kr.hhplus.be.server.config.web;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class LoggingFilter extends OncePerRequestFilter{
+
+    private final ObjectMapper objectMapper;
+
+    public LoggingFilter(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
+
+        long startTime = System.currentTimeMillis();
+
+        try {
+            filterChain.doFilter(requestWrapper, responseWrapper);
+        } finally {
+            logRequest(requestWrapper);
+
+            long duration = System.currentTimeMillis() - startTime;
+            logResponse(responseWrapper, duration);
+
+            responseWrapper.copyBodyToResponse();
+        }
+    }
+
+    private void logRequest(ContentCachingRequestWrapper request) {
+        String ip = request.getRemoteAddr();
+        String method = request.getMethod();
+        String url = request.getRequestURL().toString();
+        String requestBody = getContent(request.getContentAsByteArray());
+
+        log.info("[REQUEST] IP: {}, Method: {}, URL: {}, Body: {}", ip, method, url, requestBody);
+    }
+
+    private void logResponse(ContentCachingResponseWrapper response, long duration) {
+        int status = response.getStatus();
+        String responseBody = getContent(response.getContentAsByteArray());
+
+        log.info("[RESPONSE] Duration: {}ms, Status: {}, Body: {}", duration, status, responseBody);
+    }
+
+    private String getContent(byte[] content) {
+        if (content.length > 0) {
+            return new String(content, StandardCharsets.UTF_8);
+        }
+        return "[EMPTY]";
+    }
+    
+}
