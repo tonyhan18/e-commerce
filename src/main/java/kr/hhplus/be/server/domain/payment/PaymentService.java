2f47fdd74fbd1e7e7ff3f0a4c49e21f342454d6c
tonyhan18 <ourclub7279@gmail.com>
2025-08-31 14:00:07 +0900

    REFACTOR : Payment 결제 취소 구현

--- src/main/java/kr/hhplus/be/server/domain/payment/PaymentService.java
+++ src/main/java/kr/hhplus/be/server/domain/payment/PaymentService.java
@@ -1,7 +1,7 @@
 package kr.hhplus.be.server.domain.payment;
 
 import org.springframework.stereotype.Service;
-
+import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -9,11 +9,18 @@ import lombok.RequiredArgsConstructor;
 public class PaymentService {
     private final PaymentRepository paymentRepository;
 
+    @Transactional
     public PaymentInfo.Payment pay(PaymentCommand.Payment command) {
         Payment payment = Payment.create(command.getOrderId(), command.getAmount());
         payment.pay();
-        paymentRepository.save(payment);
 
+        paymentRepository.save(payment);
         return PaymentInfo.Payment.of(payment.getId());
+    }
+
+    @Transactional
+    public void cancelPayment(Long paymentId) {
+        Payment payment = paymentRepository.findById(paymentId);
+        payment.cancel();
     }
 }
